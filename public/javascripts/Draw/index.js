87c4b72cd59bac4ba01e90b22aa2f979e49a38ce
sunjiaxiang <sunjiaxiang@gmail.com>
2012-10-28 18:50:07 +0800

    changed operaiont javascript.

---
+++ public/javascripts/Draw/index.js
@@ -0,0 +1,44 @@
+
+MT = MT || {};
+MT.components = MT.components || {};
+
+(function () {
+	
+	/** 操作列表. */
+	MT.components.Operation = function () {
+		
+	}
+	MT.components.Operation.prototype = {
+		_render: function () {
+		
+		}
+	}
+	
+	/** 绘图区. */
+	MT.components.Sketch = function () {
+		
+	}
+	MT.components.Sketch.prototype = {
+		_render: function () {
+			
+		}
+	}
+	
+	/** 设置列表. */
+	MT.components.Setting = function () {
+		
+	}
+	MT.components.Setting.prototype = {
+		_render: function () {
+			
+		}
+	}
+	
+	// 页面逻辑代码
+	$(function () {
+		var _operation = new Operation({
+			elem: 'operation',
+			width: 960;
+		});
+	});
+})();
